e7e6965dcd5dc6d6b4648d9a933932a75ea7d98b
KP Parrish <krparrish@adobe.com>
2024-12-06 02:33:59 -0700

    updated cards.js to fix services classes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,23 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
 import CardsServices from './cards-services.js';
 
 export default function decorate(block) {
+  // only cards block that doesn't have a json feeding it
+  block.classList.add('services');
+
   const isServices = block.classList.contains('services');
 
   if (isServices) {
     CardsServices(block);
-  } else {
+  }
+
+  if (!isServices) {
+    block.classList.add('default');
+
     /* change to ul, li */
     const ul = document.createElement('ul');
     [...block.children].forEach((row) => {
       const li = document.createElement('li');
-      moveInstrumentation(row, li);
       while (row.firstElementChild) li.append(row.firstElementChild);
       [...li.children].forEach((div) => {
         if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
@@ -20,11 +25,25 @@ export default function decorate(block) {
       });
       ul.append(li);
     });
-    ul.querySelectorAll('picture > img').forEach((img) => {
-      const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-      moveInstrumentation(img, optimizedPic.querySelector('img'));
-      img.closest('picture').replaceWith(optimizedPic);
+    ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+
+    /* Modify a tags */
+    ul.querySelectorAll('li').forEach((li) => {
+      const a = li.querySelector('a');
+      const { href } = a;
+
+      a.parentElement.remove();
+
+      const newA = document.createElement('a');
+      newA.href = href;
+
+      while (li.firstChild) {
+        newA.appendChild(li.firstChild);
+      }
+
+      li.appendChild(newA);
     });
+
     block.textContent = '';
     block.append(ul);
   }
